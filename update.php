812c8ea84803465fa82a2bdc12455e6324a9126a
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-10-16 18:52:07 +0200

    #1 - Fix reference to event table
    Resolves #1

--- update.php
+++ update.php
@@ -3,7 +3,7 @@
 
 // upgrade to v0.0.3
 if (rex_version::compare($this->getVersion(), '0.0.3', '<')) {
-    rex_sql_table::get(rex::getTable('naju_event'))
+    rex_sql_table::get('naju_event')
         ->ensureColumn(new rex_sql_column('event_type', "enum ('camp', 'workshop', 'work_assignment', 'group_meeting')"))
         ->alter();
 }
